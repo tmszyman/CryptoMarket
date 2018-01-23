5304ac7149974a0b23aea6861cdc1936b1923d6a
Michał Rychlewicz <michal.rychlewicz@edu.uekat.pl>
2018-01-23 13:16:12 +0100

    WalletScreen improved
    Added icons, changed account balance

--- components/Screens/MainScreen/WalletScreen/WalletScreen.js
+++ components/Screens/MainScreen/WalletScreen/WalletScreen.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { AsyncStorage, StyleSheet, Text, View, TextInput, Button, ScrollView, DeviceEventEmitter } from 'react-native';
-import {
-    StackNavigator, TabNavigator
-} from 'react-navigation';
+import { AsyncStorage, StyleSheet, Text, View, TextInput, Button, ScrollView, Image, DeviceEventEmitter } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
 export default class WalletScreen extends React.Component {
     constructor(props) {
@@ -23,9 +21,12 @@ export default class WalletScreen extends React.Component {
 
     render() {
         const listCryptocurrencies = this.state.player.wallet.cryptocurrencies.map((cryptocurrency, key) => {
+            let icon = 'https://files.coinmarketcap.com/static/img/coins/64x64/'+cryptocurrency.name.toLowerCase()+'.png';   
             return (
-                <View key={key} style={{ height: 40, justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 16, flexDirection: 'row' }}>{cryptocurrency.name}: {cryptocurrency.amount}</Text>
+                
+                <View key={key} style={{ height: 60, justifyContent: 'flex-start', flexDirection: 'row', padding: 5 , alignItems:'center'}}>
+                    <Image source={{ uri: icon }} style={{ width: 40, height: 40, borderRadius: 50}} />
+                    <Text style={{ fontSize: 18, textAlignVertical: 'center', marginLeft: 10, borderBottomWidth: 1, borderBottomColor: '#ececec'}}>{cryptocurrency.name}: {cryptocurrency.amount}</Text>
                 </View>
             );
         });
@@ -35,9 +36,12 @@ export default class WalletScreen extends React.Component {
                 <View style={{ paddingTop: 8 }}>
                     <View style={{ height: 88, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#ececec', paddingLeft: 16, paddingRight: 16 }}>
                         <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Witaj, {this.state.player.name}</Text>
-                        <Text style={{ fontSize: 12 }}>Twoje saldo: {this.state.player.wallet.currencies[0].amount} {this.props.screenProps.player.wallet.currencies[0].name}</Text>
+                        <View style={{ flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 12 }}>Twoje saldo:</Text>
+                            <Text style={{ fontSize: 12 }}>{this.state.player.wallet.currencies[0].amount} {this.props.screenProps.player.wallet.currencies[0].name}</Text>
+                        </View>
                     </View>
-                    <View style={{ paddingTop: 8, paddingLeft: 16, paddingRight: 16 }}>
+                    <View style={{ paddingTop: 8, paddingLeft: 16, paddingRight: 16, flexDirection: 'column' }}>
                         {listCryptocurrencies}
                     </View>
                 </View>
